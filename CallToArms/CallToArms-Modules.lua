811dd1aa7bf757162a33562b167ad9477b42ffa1
Hydra <ryanandrews135@gmail.com>
2020-04-22 14:23:53 -0600

    Remove a debug print

--- CallToArms/CallToArms-Modules.lua
+++ CallToArms/CallToArms-Modules.lua
@@ -5,7 +5,7 @@ local Level = UnitLevel("player")
 -- Find dungeons
 for i = 1, GetNumRandomDungeons() do
 	ID, Name, SubType, _, _, _, Min, Max = GetLFGRandomDungeonInfo(i)
-	print(Name, SubType)
+	
 	if (Level >= Min and Level <= Max) then
 		CallToArmsGlobal:NewModule(ID, Name, SubType)
 	end
